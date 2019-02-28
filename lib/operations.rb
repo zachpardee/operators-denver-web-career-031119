1f5495f8891148a37e09ded809b30e64d0c45f71
awesome-edition-6075 <zachpardee@gmail.com>
2019-02-28 09:25:36 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,14 @@
 def unsafe?(speed)
-  if speed > 60
+  if speed > 60 || speed < 40
     true
-  elsif speed < 40
-    true
-  else 
+  else
     false
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : false
 end
 	
 
